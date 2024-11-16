6e105f133670261da0c9c960ec2113dc10908448
Luis Masuelli <luisfmasuelli@gmail.com>
2024-11-16 15:06:40 -0500

    Starting everything

---
+++ addons/AlephVault.MMO/scripts/client.gd
@@ -0,0 +1,80 @@
+extends Node
+
+class_name AVMMOClient
+
+## The signal triggered when the client connected to a server.
+signal client_started
+
+## The signal triggered when the client disconnected from a server.
+signal client_stopped
+
+# The current address from the current launch.
+var _address: String
+
+# The current port from the current launch.
+var _port: int
+
+## The current address from the current launch.
+var address: String:
+	get:
+		return _address
+
+## The current port from the current launch.
+var port: int:
+	get:
+		return _port
+
+## Joins a server.
+##
+## All the parameters are forwarded to create_client.
+func join_server(
+	address: String, port: int, channel_count: int = 0,
+	in_bandwidth: int = 0, out_bandwidth: int = 0, local_port: int = 0
+) -> Error:
+	"""
+	Joins a server.
+	
+	All the parameters are forwarded to create_client.
+	"""
+
+	var peer = ENetMultiplayerPeer.new()
+	var err: Error = peer.create_client(
+		address, port, channel_count,
+		in_bandwidth, out_bandwidth, local_port
+	)
+	if err != null:
+		return err
+	multiplayer.multiplayer_peer = peer
+	_address = address
+	_port = port
+	multiplayer.connected_to_server.connect(_on_connected_to_server)
+	multiplayer.server_disconnected.connect(_on_server_disconnected)
+	return OK
+
+## Leaves the current server.
+##
+## Returns true if the client could be stopped, or false if it could not.
+func leave_server() -> bool:
+	"""
+	Leaves the server (stops the client).
+	"""
+
+	if multiplayer.is_server():
+		return false
+
+	if not multiplayer.has_multiplayer_peer():
+		return false
+	
+	var status: MultiplayerPeer.ConnectionStatus = multiplayer.multiplayer_peer.get_connection_status()
+	if status != MultiplayerPeer.ConnectionStatus.CONNECTION_CONNECTED:
+		return false
+		
+	multiplayer.multiplayer_peer.close()
+	multiplayer.multiplayer_peer = null
+	return true
+
+func _on_connected_to_server():
+	client_started.emit()
+
+func _on_server_disconnected():
+	client_stopped.emit()
---
+++ addons/AlephVault.MMO/scripts/server.gd
@@ -0,0 +1,88 @@
+extends Node
+
+class_name AVMMOServer
+
+## The signal triggered when the server starts.
+signal server_started
+
+## The signal triggered when the server is stopped.
+signal server_stopped
+
+## The signal triggered when a client entered.
+signal client_entered(id: int)
+
+## The signal triggered when a client left.
+signal client_left(id: int)
+
+# The current address from the current launch.
+var _address: String
+
+# The current port from the current launch.
+var _port: int
+
+## The current address from the current launch.
+var address: String:
+	get:
+		return _address
+
+## The current port from the current launch.
+var port: int:
+	get:
+		return _port
+
+## Launches a server.
+##
+## All the parameters are forwarded to set_bind_ip
+## or create_server, respectively.
+func launch(
+	port: int, max_clients: int = 4096, max_channels: int = 0,
+	in_bandwidth: int = 0, out_bandwidth: int = 0, address: String = "*"
+) -> Error:
+	"""
+	Launches a server.
+	
+	All the parameters are forwarded to set_bind_ip
+	or create_server, respectively.
+	"""
+	
+	var peer = ENetMultiplayerPeer.new()
+	peer.set_bind_ip(address)
+	var err: Error = peer.create_server(
+		port, max_clients, max_clients,
+		in_bandwidth, out_bandwidth
+	)
+	if err != null:
+		return err
+	multiplayer.multiplayer_peer = peer
+	_address = address
+	_port = port
+	multiplayer.peer_connected.connect(_on_peer_connected)
+	multiplayer.peer_disconnected.connect(_on_peer_disconnected)
+	return OK
+
+## Stops a server.
+##
+## Returns true if the server could be stopped, or false if it could not.
+func stop() -> bool:
+	"""
+	Stops a server.
+	"""
+	
+	if multiplayer.is_server():
+		_address = ""
+		_port = 0
+		multiplayer.multiplayer_peer.close()
+		multiplayer.multiplayer_peer = null
+		server_stopped.emit()
+		return true
+	return false
+
+func _on_peer_connected(id: int):
+	if id == 1:
+		server_started.emit()
+	else:
+		client_entered.emit(id)
+
+func _on_peer_disconnected(id: int):
+	if id != 1:
+		client_left.emit(id)
