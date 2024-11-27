f029e9a33d0094b6fd4291ad0ad87ce8dce8cf2b
Luis Masuelli <luisfmasuelli@gmail.com>
2024-11-26 21:18:47 -0500

    Starting a function to add the connection to the list of connections

--- addons/AlephVault.MMO/scripts/client/connections.gd
+++ addons/AlephVault.MMO/scripts/client/connections.gd
@@ -1,3 +1,18 @@
 extends Node
 
 class_name AVMMOClientConnections
+
+# The connections will be kept here. Actually, only
+# one connection will belong here.
+var _connections: Dictionary = {}
+
+## Adds a new connection object for the given
+## connection id. It will be the only one here.
+func add_client(id: int) -> AVMMOClientConnection:
+	# Create the node.
+	var node = AVMMOClientConnection.new()
+	node.name = "Connection.%s" % id
+	add_child(node, true)
+	
+	# Return the node.
+	return node
--- addons/AlephVault.MMO/scripts/server/connections.gd
+++ addons/AlephVault.MMO/scripts/server/connections.gd
@@ -1,3 +1,17 @@
 extends Node
 
 class_name AVMMOServerConnections
+
+# The connections will be kept here.
+var _connections: Dictionary = {}
+
+## Adds a new connection object for the given
+## connection id.
+func add_client(id: int) -> AVMMOServerConnection:
+	# Create the node.
+	var node = AVMMOServerConnection.new()
+	node.name = "Connection.%s" % id
+	add_child(node, true)
+	
+	# Return the node.
+	return node
