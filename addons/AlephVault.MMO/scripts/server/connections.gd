f20ec83d663516ddd2d08a5bff740fdeb69f2cd4
Luis Masuelli <luisfmasuelli@gmail.com>
2024-12-22 10:39:36 -0500

    Using the utils

--- addons/AlephVault.MMO/scripts/server/connections.gd
+++ addons/AlephVault.MMO/scripts/server/connections.gd
@@ -16,52 +16,6 @@ const SCOPE_LIMBO: int = 0
 ## in order to start playing.
 const SCOPE_ACCOUNT_DASHBOARD: int = 1
 
-## Defines the macro-types for scopes. This is
-## about structures and life-cycles of scopes,
-## rather than their implementations.
-enum ScopeType {
-	## Scopes that are defined on server startup.
-	## These scopes cannot be unloaded or undefined
-	## until the server is stopped.
-	DEFAULT=0,
-	## Scopes that are defined/loaded on demand, and
-	## are unloaded on demand as well. Remaining ones
-	## are unloaded when the server is stopped.
-	DYNAMIC=1,
-	## Special scopes. They don't have any associated
-	## actual object but represent some sort of state
-	## deemed intermediate / non-playing (e.g. limbo,
-	## choosing account, ...).
-	SPECIAL=2
-}
-
-## Computes a final scope id, given the partial id and
-## the scope type.
-static func make_fq_scope_id(id: int, scope_type: ScopeType) -> int:
-	if id < 0 || id >= (1 << 30):
-		return -1
-	if not ScopeType.has(scope_type):
-		return -1
-	return int(scope_type) << 30 || id
-
-## Computes a final default scope id, given the partial id.
-static func make_fq_default_scope_id(id: int) -> int:
-	return make_fq_scope_id(id, ScopeType.DEFAULT)
-
-## Computes a final dynamic scope id, given the partial id.
-static func make_fq_dynamic_scope_id(id: int) -> int:
-	return make_fq_scope_id(id, ScopeType.DYNAMIC)
-
-## Computes a final scpecial scope id, given the partial id.
-static func make_fq_special_scope_id(id: int) -> int:
-	return make_fq_scope_id(id, ScopeType.SPECIAL)
-
-## Unpacks a final scope id into its sub-id and type.
-static func unpack_scope_id(id: int) -> Dictionary:
-	if id < 0:
-		return {}
-	return {"id": id & ((1 << 30) - 1), "type": ScopeType.get(id >> 30)}
-
 func _inherits_native_class(script: Script, native_class_name: String) -> bool:
 	# If the script's top-level extends statement is "extends Node",
 	# script.native_class == "Node"
@@ -82,7 +36,7 @@ var connection_class: Script = AVMMOServerConnection:
 			connection_class = value
 
 func _add_special_scope(id: int) -> Dictionary:
-	id = make_fq_special_scope_id(id)
+	id = AVMMOScopes.make_fq_special_scope_id(id)
 	if _scopes.has(id):
 		return _scopes[id]
 	else:
@@ -123,7 +77,7 @@ func _unset_connection_from_scope(connection_id: int):
 			var scope = _scopes[scope_id]
 			if scope.has(connection_id):
 				scope.erase(connection_id)
-			if len(scope) == 0 and ScopeType.get(scope_id >> 30) != ScopeType.SPECIAL:
+			if len(scope) == 0 and AVMMOScopes.ScopeType.get(scope_id >> 30) != AVMMOScopes.ScopeType.SPECIAL:
 				_scopes.erase(scope_id)
 
 func _unset_connection_scope(connection_id: int):
@@ -177,7 +131,7 @@ func add_client(id: int) -> AVMMOServerConnection:
 	var node = connection_class.new()
 	node.name = "Connection.%s" % id
 	node.id = id
-	_set_connection_scope(id, make_fq_special_scope_id(SCOPE_LIMBO))
+	_set_connection_scope(id, AVMMOScopes.make_fq_special_scope_id(SCOPE_LIMBO))
 	add_child(node, true)
 	
 	# Return the node.
