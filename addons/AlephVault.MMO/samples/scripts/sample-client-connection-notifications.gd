cf104c7d9ab577055c0825e6892df4f90d2f1856
Luis Masuelli <luisfmasuelli@gmail.com>
2024-12-26 16:18:46 -0500

    Adding some notifications from the client notifications object to the ui

--- addons/AlephVault.MMO/samples/scripts/sample-client-connection-notifications.gd
+++ addons/AlephVault.MMO/samples/scripts/sample-client-connection-notifications.gd
@@ -2,20 +2,16 @@ extends AVMMOClientConnectionNotifications
 
 @rpc("authority", "call_remote", "reliable")
 func user_join(connection_id: int, nick: String):
-	# TODO implement.
-	pass
+	$"../../..".client_ui.message_user_join(connection_id, nick)
 
 @rpc("authority", "call_remote", "reliable")
 func user_part(connection_id: int, nick: String):
-	# TODO implement.
-	return false
+	$"../../..".client_ui.message_user_part(connection_id, nick)
 
 @rpc("authority", "call_remote", "reliable")
 func user_sent(connection_id: int, nick: String, message: String):
-	# TODO implement.
-	return false
+	$"../../..".client_ui.message_user_sent(connection_id, nick, message)
 
 @rpc("authority", "call_remote", "reliable")
 func user_nick(connection_id: int, nick: String, new_nick: String):
-	# TODO implement.
-	pass
+	$"../../..".client_ui.message_user_nickn(connection_id, nick, new_nick)
