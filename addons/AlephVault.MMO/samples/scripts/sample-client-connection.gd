22ddf7312fba862d76dde2175f81e57cc0e2a65c
Luis Masuelli <luisfmasuelli@gmail.com>
2024-12-26 11:58:25 -0500

    Adding empty script classes for the connection

---
+++ addons/AlephVault.MMO/samples/scripts/sample-client-connection.gd
@@ -0,0 +1,16 @@
+extends AVMMOClientConnection
+
+var _commands_class = preload("./sample-client-connection-commands.gd")
+var _notifications_class = preload("./sample-client-connection-notifications.gd")
+
+func _make_commands_node() -> AVMMOClientConnectionCommands:
+	# Override this to instantiate the node serving the
+	# commands that are issued to the server. Other than
+	# that, the commands are implemented through RPC.
+	return _commands_class.new()
+
+func _make_notifications_mode() -> AVMMOClientConnectionNotifications:
+	# Override this to instantiate the node serving the
+	# notifications to the client. Other than that, the
+	# notifications are implemented through RPC.
+	return _notifications_class.new()
---
+++ addons/AlephVault.MMO/samples/scripts/sample-server-connection.gd
@@ -0,0 +1,16 @@
+extends AVMMOServerConnection
+
+var _commands_class = preload("./sample-server-connection-commands.gd")
+var _notifications_class = preload("./sample-server-connection-notifications.gd")
+
+func _make_commands_node() -> AVMMOServerConnectionCommands:
+	# Override this to instantiate the node serving the
+	# commands that are issued to the server. Other than
+	# that, the commands are implemented through RPC.
+	return _commands_class.new()
+
+func _make_notifications_mode() -> AVMMOServerConnectionNotifications:
+	# Override this to instantiate the node serving the
+	# notifications to the client. Other than that, the
+	# notifications are implemented through RPC.
+	return _notifications_class.new()
