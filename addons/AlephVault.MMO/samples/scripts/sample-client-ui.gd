54fd7650bfb4ec45bfaf54ac993e2791496fdacf
Luis Masuelli <luisfmasuelli@gmail.com>
2024-12-26 13:04:43 -0500

    Adding some notification implementation in the client ui

--- addons/AlephVault.MMO/samples/scripts/sample-client-ui.gd
+++ addons/AlephVault.MMO/samples/scripts/sample-client-ui.gd
@@ -28,3 +28,46 @@ func send_command():
 	else:
 		# Sends a message.
 		pass
+
+func _add_line(line: String):
+	var text: String = %Message.text
+	var new_text: String = line
+	if text.strip_edges() != "":
+		new_text = text + "\n" + line
+	%Message.text = new_text
+
+func _clear():
+	%Message.text = ""
+
+func message_connection_started():
+	_add_line("## Connection started")
+
+func message_connection_failed():
+	_add_line("## Connection failed")
+
+func message_connection_closed():
+	_add_line("## Connection closed")
+
+func message_scope_changed(id: int):
+	_add_line("## [DEBUG] Scope changed: %d" % id)
+
+func message_local_nick(nick: String, result: bool):
+	if result:
+		_add_line("** Nick updated successfully: " + nick)
+	else:
+		_add_line("** ! Could not update nick: " + nick)
+
+func message_local_list(result: Array[String]):
+	_add_line("** Available channels: " + ", ".join(result))
+
+func message_local_join(channel: String, result: bool):
+	if result:
+		_add_line("** Joining channel: " + channel)
+	else:
+		_add_line("** ! Could not join channel: " + channel)
+
+func message_local_part(result: bool):
+	if result:
+		_add_line("** Parting current channel")
+	else:
+		_add_line("** ! Could not part current channel")
