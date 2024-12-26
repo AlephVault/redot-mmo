6c96ae3631bed4572d771b129797894b81e26924
Luis Masuelli <luisfmasuelli@gmail.com>
2024-12-26 12:32:30 -0500

    Adding the notifications (not implemented)

--- addons/AlephVault.MMO/samples/scripts/sample-client-connection-notifications.gd
+++ addons/AlephVault.MMO/samples/scripts/sample-client-connection-notifications.gd
@@ -1,1 +1,21 @@
 extends AVMMOClientConnectionNotifications
+
+@rpc("authority", "call_remote", "reliable")
+func user_join(connection_id: int, nick: String):
+	# TODO implement.
+	pass
+
+@rpc("authority", "call_remote", "reliable")
+func user_part(connection_id: int, nick: String) -> bool:
+	# TODO implement.
+	return false
+
+@rpc("authority", "call_remote", "reliable")
+func user_sent(message: String) -> bool:
+	# TODO implement.
+	return false
+
+@rpc("authority", "call_remote", "reliable")
+func user_nick(nick: String):
+	# TODO implement.
+	pass
--- addons/AlephVault.MMO/samples/scripts/sample-server-connection-notifications.gd
+++ addons/AlephVault.MMO/samples/scripts/sample-server-connection-notifications.gd
@@ -1,1 +1,17 @@
 extends AVMMOServerConnectionNotifications
+
+@rpc("authority", "call_remote", "reliable")
+func user_join(connection_id: int, nick: String):
+	pass
+
+@rpc("authority", "call_remote", "reliable")
+func user_part(connection_id: int, nick: String) -> bool:
+	return false
+
+@rpc("authority", "call_remote", "reliable")
+func user_sent(message: String) -> bool:
+	return false
+
+@rpc("authority", "call_remote", "reliable")
+func user_nick(nick: String):
+	pass
