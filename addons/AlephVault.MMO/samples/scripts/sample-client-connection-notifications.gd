bc58d6d11e1789c08125a7a1ce6cf58d7fecc177
Luis Masuelli <luisfmasuelli@gmail.com>
2024-12-26 12:44:31 -0500

    Changing some notifications

--- addons/AlephVault.MMO/samples/scripts/sample-client-connection-notifications.gd
+++ addons/AlephVault.MMO/samples/scripts/sample-client-connection-notifications.gd
@@ -6,16 +6,16 @@ func user_join(connection_id: int, nick: String):
 	pass
 
 @rpc("authority", "call_remote", "reliable")
-func user_part(connection_id: int, nick: String) -> bool:
+func user_part(connection_id: int, nick: String):
 	# TODO implement.
 	return false
 
 @rpc("authority", "call_remote", "reliable")
-func user_sent(message: String) -> bool:
+func user_sent(connection_id: int, nick: String, message: String):
 	# TODO implement.
 	return false
 
 @rpc("authority", "call_remote", "reliable")
-func user_nick(nick: String):
+func user_nick(connection_id: int, nick: String, new_nick: String):
 	# TODO implement.
 	pass
--- addons/AlephVault.MMO/samples/scripts/sample-server-connection-notifications.gd
+++ addons/AlephVault.MMO/samples/scripts/sample-server-connection-notifications.gd
@@ -5,13 +5,13 @@ func user_join(connection_id: int, nick: String):
 	pass
 
 @rpc("authority", "call_remote", "reliable")
-func user_part(connection_id: int, nick: String) -> bool:
+func user_part(connection_id: int, nick: String):
 	return false
 
 @rpc("authority", "call_remote", "reliable")
-func user_sent(message: String) -> bool:
+func user_sent(connection_id: int, nick: String, message: String):
 	return false
 
 @rpc("authority", "call_remote", "reliable")
-func user_nick(nick: String):
+func user_nick(connection_id: int, nick: String, new_nick: String):
 	pass
