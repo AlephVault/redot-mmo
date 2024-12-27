75d11eaa7f41833a5d13552046c93e321c2a4b78
Luis Masuelli <luisfmasuelli@gmail.com>
2024-12-27 12:17:01 -0500

    In the server connection's commands, adding a reference to the server connection

--- addons/AlephVault.MMO/scripts/server/connections/commands.gd
+++ addons/AlephVault.MMO/scripts/server/connections/commands.gd
@@ -1,3 +1,15 @@
 extends Node
 
 class_name AVMMOServerConnectionCommands
+
+func _enter_tree() -> void:
+	_connection = get_parent() as AVMMOServerConnection
+
+var _connection: AVMMOServerConnection
+
+## Gets the connection node.
+var connection: AVMMOServerConnection:
+	get:
+		return _connection
+	set(value):
+		assert(false, "The connection node cannot be set this way")
