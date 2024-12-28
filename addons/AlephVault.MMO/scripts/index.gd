fb78f88bab6f338b97b52fb3a139b185664f196f
Luis Masuelli <luisfmasuelli@gmail.com>
2024-12-28 11:40:36 -0500

    Starting an index file

---
+++ addons/AlephVault.MMO/scripts/index.gd
@@ -0,0 +1,17 @@
+extends Object
+
+class_name AlephVault__MMO
+
+class Client:
+	const Main = preload("./client/main.gd")
+	const Connections = preload("./client/connections.gd")
+	const Connection = preload("./client/connections/connection.gd")
+	const ConnectionCommands = preload("./client/connections/commands.gd")
+	const ConnectionNotifications = preload("./client/connections/notifications.gd")
+
+class Server:
+	const Main = preload("./server/main.gd")
+	const Connections = preload("./server/connections.gd")
+	const Connection = preload("./server/connections/connection.gd")
+	const ConnectionCommands = preload("./server/connections/commands.gd")
+	const ConnectionNotifications = preload("./server/connections/notifications.gd")
