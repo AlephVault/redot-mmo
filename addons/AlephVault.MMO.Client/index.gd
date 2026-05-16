823805239d36a75e7792631398f10d1f060a840a
Luis Masuelli <luisfmasuelli@gmail.com>
2026-05-16 17:29:45 -0500

    MOVING the .Client contents out of the client/ subdirectory.

--- addons/AlephVault.MMO.Client/index.gd
+++ addons/AlephVault.MMO.Client/index.gd
@@ -2,10 +2,10 @@ extends Object
 
 class_name AlephVault__MMO__Client
 
-const Main = preload("./client/main.gd")
-const Connections = preload("./client/connections.gd")
-const Connection = preload("./client/connections/connection.gd")
-const ConnectionCommands = preload("./client/connections/commands.gd")
-const ConnectionNotifications = preload("./client/connections/notifications.gd")
-const World = preload("./client/world.gd")
-const Scope = preload("./client/scope.gd")
+const Main = preload("./main.gd")
+const Connections = preload("./connections.gd")
+const Connection = preload("./connections/connection.gd")
+const ConnectionCommands = preload("./connections/commands.gd")
+const ConnectionNotifications = preload("./connections/notifications.gd")
+const World = preload("./world.gd")
+const Scope = preload("./scope.gd")
