0921f2277f41bebb96b857cbd89a4c14513445d4
Luis Masuelli <luisfmasuelli@gmail.com>
2026-05-17 18:22:34 -0500

    Starting the protocols.gd parent class in both sides.

---
+++ addons/AlephVault.MMO.Server/protocols/protocol.gd
@@ -0,0 +1,14 @@
+extends Node
+
+## Implement this method to define the dependencies
+## of the current protocol. Do it in terms of classes
+## extending AlephVault__MMO__Server.Protocol.
+static func _get_dependencies(): Array[Script]:
+	return []
+
+## The protocol classes this protocol depends on.
+static var dependencies: Array[Script]:
+	get:
+		return _get_dependencies()
+	set(value):
+		assert(false, "The server's protocol dependencies cannot be set this way")
