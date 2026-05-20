224312ed4cd49a7499d3139329aba5e661e568bd
Luis Masuelli <luisfmasuelli@gmail.com>
2026-05-20 17:18:32 -0500

    The notifications (in the client) and the commands (in the server) have ways to retrieve the related connection and the related (core, global) protocol.

--- addons/AlephVault.MMO.Client/protocols/notifications.gd
+++ addons/AlephVault.MMO.Client/protocols/notifications.gd
@@ -4,3 +4,13 @@ extends Node
 ##
 ## It is highly recommended that subclasses declare all RPC methods with:
 ## @rpc("authority", "call_remote", "reliable")
+
+## Gets the current client connection node.
+func connection_node() -> AlephVault__MMO__Client.Connection:
+	return $"../.." as AlephVault__MMO__Client.Connection
+
+## Gets the protocol instance this notifications node belongs to.
+func protocol_node() -> AlephVault__MMO__Client.Protocols.Protocol:
+	var protocol_name := str(get_parent().name)
+	# Rationale: connection protocol -> protocols node -> connection -> client node -> protocols node -> the protocol.
+	return get_node_or_null("../../../../Protocols/%s" % protocol_name) as AlephVault__MMO__Client.Protocols.Protocol
--- addons/AlephVault.MMO.Server/protocols/commands.gd
+++ addons/AlephVault.MMO.Server/protocols/commands.gd
@@ -4,3 +4,13 @@ extends Node
 ##
 ## It is highly recommended that subclasses declare all RPC methods with:
 ## @rpc("authority", "call_remote", "reliable")
+
+## Gets the server connection node this commands node belongs to.
+func connection_node() -> AlephVault__MMO__Server.Connection:
+	return $"../.." as AlephVault__MMO__Server.Connection
+
+## Gets the protocol instance this commands node belongs to.
+func protocol_node() -> AlephVault__MMO__Server.Protocols.Protocol:
+	var protocol_name := str(get_parent().name)
+	# Rationale: connection protocol -> protocols node -> connection -> server node -> protocols node -> the protocol.
+	return get_node_or_null("../../../../Protocols/%s" % protocol_name) as AlephVault__MMO__Server.Protocols.Protocol
