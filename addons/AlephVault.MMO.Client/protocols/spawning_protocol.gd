3378b7f2c00fc320c76030de891da27549e48c75
Luis Masuelli <luisfmasuelli@gmail.com>
2026-05-23 15:56:20 -0500

    Adding the Spawning Protocol.

---
+++ addons/AlephVault.MMO.Client/protocols/spawning_protocol.gd
@@ -0,0 +1,41 @@
+extends AlephVault__MMO__Client.Protocols.Protocol
+
+var _world: Node
+var _spawner: MultiplayerSpawner
+
+func _enter_tree() -> void:
+	if not (get_parent() is AlephVault__MMO__Client.Protocols.Manager):
+		return
+
+	var world := _create_world()
+	if world == null:
+		return
+
+	world.name = "World"
+	add_child(world)
+	_world = world
+
+	var spawner := MultiplayerSpawner.new()
+	spawner.name = "MultiplayerSpawner"
+	_setup_spawner(spawner)
+	add_child(spawner)
+	_spawner = spawner
+
+func _exit_tree() -> void:
+	if _world != null:
+		remove_child(_world)
+		_world.queue_free()
+		_world = null
+
+	if _spawner != null:
+		remove_child(_spawner)
+		_spawner.queue_free()
+		_spawner = null
+
+## Override this to instantiate the world node used by this spawning protocol.
+func _create_world() -> Node:
+	return null
+
+## Override this to configure the MultiplayerSpawner used by this spawning protocol.
+func _setup_spawner(s: MultiplayerSpawner) -> void:
+	pass
