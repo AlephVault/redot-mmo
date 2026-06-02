e55c1f8730905a2e5d4306e54e58421652dd9404
Luis Masuelli <luisfmasuelli@gmail.com>
2026-06-01 22:34:19 -0500

    Removing the Simple Authentication section from Common.

--- addons/AlephVault.MMO.Common/protocols/authentication/index.gd
+++ addons/AlephVault.MMO.Common/protocols/authentication/index.gd
@@ -4,4 +4,3 @@ const AccountAlreadyLoggedManagementMode = preload("./account_already_logged_man
 const KickReasons = preload("./kick_reasons.gd")
 const LoginResult = preload("./login_result.gd")
 const SessionStage = preload("./session_stage.gd")
-const Simple = preload("./simple/index.gd")
