5744785506f02557cad5e44fd1661bfef10d7c4a
Luis Masuelli <luisfmasuelli@gmail.com>
2026-06-18 16:57:46 -0500

    Adding the Types' index.gd file.

--- addons/AlephVault.MMO.Storage/index.gd
+++ addons/AlephVault.MMO.Storage/index.gd
@@ -2,18 +2,5 @@ extends Object
 
 class_name AlephVault__MMO__Storage
 
-## Public namespace for storage result types.
-class Types:
-	## Result code enum and constants.
-	const ResultCode = preload("./types/results/result_code.gd")
-	## Result object returned by resource operations.
-	const Result = preload("./types/results/result.gd")
-
-	## Compatibility namespace matching the result-type folder.
-	class Results:
-		## Result code enum and constants.
-		const ResultCode = preload("./types/results/result_code.gd")
-		## Result object returned by resource operations.
-		const Result = preload("./types/results/result.gd")
-
+const Types = preload("./types/index.gd")
 const StandardHttp = preload("./standard_http/index.gd")
---
+++ addons/AlephVault.MMO.Storage/types/index.gd
@@ -0,0 +1,4 @@
+extends Object
+
+const ResultCode = preload("./results/result_code.gd")
+const Result = preload("./results/result.gd")
